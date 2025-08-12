4ffe0281000766793576491d8027dc14de88d228
Andrey Tsarev <77644246+tsarev2709@users.noreply.github.com>
2025-08-12 13:39:51 +0300

    Configure GitHub Pages deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.BASE_PATH || ''
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
 }
 
 module.exports = nextConfig
